8616e1da535300a735c3065116b5d03bad290dd3
unknown <adrien.sablayrolles@lacatholille.fr>
2024-12-17 16:31:19 +0100

    Commit

--- tests/Leonardo.Tests/UnitTest1.cs
+++ tests/Leonardo.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Leonardo.Tests;
 
 public class UnitTest1
@@ -5,7 +7,13 @@ public class UnitTest1
     [Fact]
     public async Task Test1()
     {
-        var results = await Fibonacci.RunAsync(new string[]{"41"});
+        var builder = new DbContextOptionsBuilder<FibonacciDataContext>(); 
+        var dataBaseName = Guid.NewGuid().ToString(); 
+        builder.UseInMemoryDatabase(dataBaseName);  
+        var options = builder.Options; 
+        var fibonacciDataContext = new FibonacciDataContext(options); 
+        await fibonacciDataContext.Database.EnsureCreatedAsync(); 
+        var results = await new Fibonacci(fibonacciDataContext).RunAsync(new string[]{ "41" });
         Assert.Equal(165580141, results[0].Result);
     }
 }
